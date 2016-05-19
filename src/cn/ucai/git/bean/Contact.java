b128703a882b93218a98355c0a5e9da7a1bbfa70
402034990 <402034990@qq.com>
2016-05-19 15:10:32 +0800

    给mContactId添加equals方法

--- src/cn/ucai/git/bean/Contact.java
+++ src/cn/ucai/git/bean/Contact.java
@@ -75,6 +75,22 @@ public class Contact extends User implements java.io.Serializable {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Contact contact = (Contact) o;
+
+		return mcontactId.equals(contact.mcontactId);
+
+	}
+
+	@Override
+	public int hashCode() {
+		return mcontactId.hashCode();
+	}
+
+	@Override
 	public String toString() {
 		return "Contact [MContactId=" + mcontactId + ", MContactUserId="
 				+ mcontactUserId + ", MContactUserName=" + mcontactUserName
